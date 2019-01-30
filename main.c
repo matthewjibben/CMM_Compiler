c7379638d34898caaf63fa07f415e44fb275fff6
Matthew Jibben <matthewjibben@gmail.com>
2019-01-30 06:33:33 -0700

    Improved symbol table so each environment is a doubly linked list of symbols  * Added insert, delete, and print  * Updated lookup

--- main.c
+++ main.c
@@ -1,109 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mem.h>
 #include "symboltable.h"
 
-/*
-// =======================   Symbol    =======================
-typedef struct Symbol {
-    // the 2d array includes 3 elements in each row:
-    // 0: identifier
-    // 1: type
-    // 2: value pointer. This will be able to be updated in the future as needed
-    char* id;
-    char* type;
-    char* vpointer;
-    //I use a doubly linked list of symbols so that deletion is easier
-    struct Symbol* next;
-    struct Symbol* prev;
-};
-// =======================   Symbol    =======================
-
-// ======================= Environment =======================
-typedef struct Env {
-    int size;
-    struct Symbol* table;
-    struct Env* prev;
-
-    // MAKE AN ARRAY OF POINTERS TO SYMBOLS
-    // THAT WAY YOU ONLY HAVE TO ADD ANOTHER 4 BYTES IF YOU NEED A NEW POINTER
-};
-
-void initEnv(struct Env* env, struct Env* previous){
-    //set the size to 0, it will be changed once we make insertions
-    env->size = 0;
-    //The table currently has no elements, the array will be increased as needed
-    env->table = malloc(0);
-    env->prev = previous;
-}
-
-int insertEntry(struct Env* env, char* id, char* type, char* vpointer){
-    // allocate the size of the original memory plus enough space for one more pointer
-    int symSize = sizeof(struct Symbol);
-    void* newMem = realloc(env->table, (size_t)(symSize*env->size + symSize));
-    if(!newMem){
-        printf("allocating memory somehow failed");
-        abort();
-    }
-    env->table = newMem;
-
-    struct Symbol temp = {id, type, vpointer};
-
-    //printf("size of temp: %i\n", sizeof(struct Symbol));
-    env->table[env->size] = temp;
-    env->size++;
-    // we will return the index in the table where it is inserted
-    return env->size-1;
-}
-
-struct Symbol* lookup(struct Env* env, char* id){
-    // lookup through each environment until we find the id
-    for(struct Env* tempenv = env; tempenv != NULL; tempenv = tempenv->prev){
-        // search through the table
-        for(int i=0; i<tempenv->size; ++i){
-            if(tempenv->table[i].id == id){
-                return &tempenv->table[i];
-            }
-        }
-    }
-    // if the value is never reached
-    return NULL;
-}
-
-void deleteItem(struct Env* env, char* id){
-    // perform the same operations as a lookup, but then delete the entry
-    // is malloc even a good idea? linked list of symbols?
-}
-
-// ======================= Environment =======================
-*/
 
 
 int main() {
     struct Env x;
+    printf("0\n");
     initEnv(&x, NULL);
+    printf("1\n");
     insertEntry(&x, "id1", "type1", "vpointer1");
+    printf("2\n");
     insertEntry(&x, "id2", "type2", "vpointer2");
-    printf("symbols: \n");
-    printf(x.table[0].id);
-    printf(x.table[1].id);
+    printf("3\n");
+    insertEntry(&x, "id3", "type3", "vpointer2");
+    printf("4\n");
+    insertEntry(&x, "id4", "type4", "vpointer2");
+    printf("5\n");
+    insertEntry(&x, "id5", "type5", "vpointer2");
+    printf("6\n");
+    printf("symbols:\n");
+    printEnv(&x);
 
-    //x.i = 211;
-    //printf("%i\n", x.i);
+    printf("main delete\n\n");
+    deleteItem(&x, "id1");
+    deleteItem(&x, "id2");
+    deleteItem(&x, "id3");
+    deleteItem(&x, "id4");
+    deleteItem(&x, "id5");
+    printEnv(&x);
 
-
-    //char arr2[] = {};
-
-
-    //char* x2 = "hello:";
-    //printf(x2);
-    //printf("%i", strlen(x2));
-
-    //void* test = malloc(5);
-    //printf("\n\n%i", sizeof(test));
-
-
-
-    free(x.table);
     return 0;
 }
--- symboltable.h
+++ symboltable.h
@@ -7,56 +7,64 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <mem.h>
 
 // =======================   Symbol    =======================
-typedef struct Symbol {
-    /* the 2d array includes 3 elements in each row:
-     * 0: identifier
-     * 1: type
-     * 2: value pointer. This will be able to be updated in the future as needed
-     */
+struct Symbol {
+    // the 2d array includes 3 elements in each row:
+    // 0: identifier
+    // 1: type
+    // 2: value pointer. This will be able to be updated in the future as needed
     char* id;
     char* type;
-    char* vpointer;
+    char* value;
     //I use a doubly linked list of symbols so that deletion is easier
     struct Symbol* next;
     struct Symbol* prev;
 };
+
+void deleteSym(struct Symbol* sym){
+    free(sym->id);
+    free(sym->type);
+    free(sym->value);
+    free(sym);
+}
 // =======================   Symbol    =======================
 
 // ======================= Environment =======================
-typedef struct Env {
+struct Env {
     int size;
-    struct Symbol* table;
+    struct Symbol* head;    // head of the linked list of symbols
+    struct Symbol* tail;
     struct Env* prev;
-
-    // MAKE AN ARRAY OF POINTERS TO SYMBOLS
-    // THAT WAY YOU ONLY HAVE TO ADD ANOTHER 4 BYTES IF YOU NEED A NEW POINTER
 };
 
 void initEnv(struct Env* env, struct Env* previous){
     //set the size to 0, it will be changed once we make insertions
     env->size = 0;
     //The table currently has no elements, the array will be increased as needed
-    env->table = malloc(0);
     env->prev = previous;
+    env->head = env->tail = NULL;
 }
 
-int insertEntry(struct Env* env, char* id, char* type, char* vpointer){
-    // allocate the size of the original memory plus enough space for one more pointer
-    int symSize = sizeof(struct Symbol);
-    void* newMem = realloc(env->table, (size_t)(symSize*env->size + symSize));
-    if(!newMem){
-        printf("allocating memory somehow failed");
-        abort();
+int insertEntry(struct Env* env, char* id, char* type, char* value){
+    struct Symbol* temp = malloc(sizeof(struct Symbol));
+    temp->id = id;
+    temp->type = type;
+    temp->value = value;
+    temp->next = NULL;
+    temp->prev = NULL;
+
+    //if this is the first symbol, set it to the head
+    if(env->head==NULL){
+        env->head = env->tail = temp;
     }
-    env->table = newMem;
-
-    struct Symbol temp = {id, type, vpointer};
-
-    //printf("size of temp: %i\n", sizeof(struct Symbol));
-    env->table[env->size] = temp;
+    else {
+        //otherwise, use the tail to add the new symbol
+        //first, link the new symbols together, then set the new symbol as the tail
+        temp->prev = env->tail;
+        env->tail->next = temp;
+        env->tail = temp;
+    }
     env->size++;
     // we will return the index in the table where it is inserted
     return env->size-1;
@@ -65,10 +73,10 @@ int insertEntry(struct Env* env, char* id, char* type, char* vpointer){
 struct Symbol* lookup(struct Env* env, char* id){
     // lookup through each environment until we find the id
     for(struct Env* tempenv = env; tempenv != NULL; tempenv = tempenv->prev){
-        // search through the table
-        for(int i=0; i<tempenv->size; ++i){
-            if(tempenv->table[i].id == id){
-                return &tempenv->table[i];
+        // search through the table by traversing the linked list
+        for(struct Symbol* temp = env->head; temp!=NULL; temp = temp->next){
+            if(temp->id == id){
+                return temp;
             }
         }
     }
@@ -78,7 +86,44 @@ struct Symbol* lookup(struct Env* env, char* id){
 
 void deleteItem(struct Env* env, char* id){
     // perform the same operations as a lookup, but then delete the entry
-    // is malloc even a good idea? linked list of symbols?
+    // lookup through each environment until we find the id
+    for(struct Env* tempenv = env; tempenv != NULL; tempenv = tempenv->prev){
+        // search through the table by traversing the linked list
+        struct Symbol* temp = env->head;
+        for(int i=0; i<env->size; ++i){
+            if(temp->id == id){
+                // connect the previous and next symbols to each other
+                if(temp->prev!=NULL) {
+                    temp->prev->next = temp->next;
+                } else {
+                    //if the previous is equal to null, we have to set a new head
+                    env->head = temp->next;
+                }
+                if(temp->next!=NULL){
+                    temp->next->prev = temp->prev;
+                } else {
+                    //if the next is equal to null, we have to set a new tail
+                    env->tail = temp->prev;
+                }
+                // delete the found symbol
+                deleteSym(temp);
+                env->size--;
+                return;
+            }
+            temp = temp->next;
+        }
+    }
+
+}
+
+void printEnv(struct Env* env){
+    printf("============Size: %i=============\n", env->size);
+    struct Symbol* temp = env->head;
+    for(int i=0; i<env->size; ++i){
+        printf("====================================\n");
+        printf("%s\t%s\t%s\n", temp->id, temp->type, temp->value);
+        temp = temp->next;
+    }
 }
 
 // ======================= Environment =======================
