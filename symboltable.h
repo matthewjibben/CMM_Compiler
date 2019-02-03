a8877abc005412e62acc24a5bbcac67843d1644e
Matthew <matthew.jibben@gmail.com>
2019-02-03 01:02:35 -0700

    Updates to symbol table and environment

--- symboltable.h
+++ symboltable.h
@@ -2,8 +2,8 @@
 // Created by Matthew Jibben on 1/29/2019.
 //
 
-#ifndef SYMBOLTABLE_SYMBOLTABLE_H
-#define SYMBOLTABLE_SYMBOLTABLE_H
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -86,54 +86,79 @@ struct Symbol* lookup(struct Env* env, char* id){
 }
 
 void deleteItem(struct Env* env, char* id){
-    // perform the same operations as a lookup, but then delete the entry
-    // lookup through each environment until we find the id
-    for(struct Env* tempenv = env; tempenv != NULL; tempenv = tempenv->prev){
-        // search through the table by traversing the linked list
-        struct Symbol* temp = env->head;
-        for(int i=0; i<env->size; ++i){
-            if(temp->id == id){
-                // connect the previous and next symbols to each other
-                if(temp->prev!=NULL) {
-                    temp->prev->next = temp->next;
-                } else {
-                    //if the previous is equal to null, we have to set a new head
-                    env->head = temp->next;
-                }
-                if(temp->next!=NULL){
-                    temp->next->prev = temp->prev;
-                } else {
-                    //if the next is equal to null, we have to set a new tail
-                    env->tail = temp->prev;
-                }
-                // delete the found symbol
-                deleteSym(temp);
-                env->size--;
-                return;
-            }
-            temp = temp->next;
+    // lookup a symbol, then delete the entry
+    struct Symbol* temp = lookup(env, id);
+    if(temp!=NULL){
+        // connect the previous and next symbols to each other
+        if(temp->prev!=NULL) {
+            temp->prev->next = temp->next;
+        } else {
+            //if the previous is equal to null, we have to set a new head
+            env->head = temp->next;
         }
+        if(temp->next!=NULL){
+            temp->next->prev = temp->prev;
+        } else {
+            //if the next is equal to null, we have to set a new tail
+            env->tail = temp->prev;
+        }
+        // delete the found symbol
+        deleteSym(temp);
+        env->size--;
     }
 
 }
 
 void printEnv(struct Env* env){
-    printf("============Size: %i=============\n", env->size);
     struct Symbol* temp = env->head;
-    for(int i=0; i<env->size; ++i){
-        printf("====================================\n");
-        printf("%s\t%s\t%s\n", temp->id, temp->type, temp->value);
-        temp = temp->next;
+    if(temp!=NULL) {
+        printf("============Size: %i=============\n", env->size);
+        for (int i = 0; i < env->size; ++i) {
+            printf("====================================\n");
+            printf("%s\t%s\t%s\n", temp->id, temp->type, temp->value);
+            temp = temp->next;
+        }
+    } else {
+        printf("=================Empty Env=================\n");
     }
 }
 
-void updateVal(struct Symbol* sym, char* newval){
-    free(sym->value);
-    sym->value = newval;
+void updateVal(struct Env* env, char* id, char* newval){
+    // use lookup to find if the id exists
+    // if it does, delete the current value and replace it with the new one
+    struct Symbol* sym = lookup(env, id);
+    if(sym!=NULL) {
+        free(sym->value);
+        sym->value = newval;
+    }
+}
+
+void updateType(struct Env* env, char* id, char* newtype){
+    // use lookup to find if the id exists
+    // if it does, delete the current value and replace it with the new one
+    struct Symbol* sym = lookup(env, id);
+    if(sym!=NULL) {
+        free(sym->value);
+        sym->type = newtype;
+    }
+}
+
+// takes as input an environment, and frees all variables held in it
+// this is used when a scope is completed
+void freeEnv(struct Env* env){
+    // search through the table by traversing the linked list
+    struct Symbol* temp;
+    while(env->head!=NULL){
+        temp = env->head;
+        env->head = env->head->next;
+        deleteSym(temp);
+    }
+    env->size = 0;
+    free(env);
 }
 
 // ======================= Environment =======================
 
 
 
-#endif //SYMBOLTABLE_SYMBOLTABLE_H
+#endif //SYMBOLTABLE_H
