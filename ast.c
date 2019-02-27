40765210aba9ea5025ed9bc209e8c0071e9f40c0
Matthew Jibben <matthewjibben@gmail.com>
2019-02-27 14:08:25 -0700

    added AST struct skeletons for declaration, statement, and expression

--- ast.c
+++ ast.c
@@ -7,6 +7,10 @@
 void initAST(AST* ast, char* value){
     ast->size = 0;
     ast->value = value;
-    ast->childHead = ast->childTail = ast->next = ast->prev = NULL;
+    ast->left = ast->right = ast->next = NULL;
 }
 
+// newAST builds an AST node with the given inputs
+AST* newAST(int num, ...){
+    // should this be used?
+}
--- ast.h
+++ ast.h
@@ -15,18 +15,51 @@ typedef struct {
 
     // pointers to the linked list of child nodes
     // having a linked list of children allows for any number of children
-    struct AST* childHead;
-    struct AST* childTail;
+    // in binary expressions, only left and right will be used
+    struct AST* left;
+    struct AST* right;
 
     // pointers to the nodes on the same level (siblings)
     struct AST* next;
-    struct AST* prev;
+    //struct AST* prev;
 } AST;
-
 
 typedef struct {
     AST* head;
 } ASTHead;
+
+
+
+typedef struct {
+    char* name;
+    int type;               // todo should there be a class to allow for many parameter types?
+    int returnType;
+    struct Expression* expr;
+    struct Statement* codeBlock;
+    struct Declaration* next;
+} Declaration;
+
+
+typedef enum {
+    STMT_IF,
+    STMT_IF_ELSE,
+    STMT_BLOCK,
+    STMT_WHILE,
+    STMT_RETRN
+} stmtType;
+
+typedef struct {
+    stmtType type;
+
+} Statement;
+
+
+
+typedef struct {
+
+} Expression;
+
+
 
 void initAST(AST* ast, char* value);
 void addChild(AST* parent, AST* child);
